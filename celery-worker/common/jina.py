1d22c86d284e702b6da7dc70e4c58e2cbe169db4
Kinda Hall <1142704468@qq.com>
2025-04-25 23:38:23 +0800

    fix: 🐛 fix the jina transform function

--- celery-worker/common/jina.py
+++ celery-worker/common/jina.py
@@ -6,7 +6,7 @@ timeout = httpx.Timeout(10.0, connect=10.0)
 def transform_website_to_markdown_by_jina(url: str):
     headers = {
         'Accept': 'application/json',
-        'Authorization': f'Bearer {os.environ.get('JINA_API_TOKEN')}'
+        'Authorization': f'Bearer {os.environ.get("JINA_API_TOKEN")}'
     }
     response = httpx.get(f'https://r.jina.ai/{url}', headers=headers, timeout=timeout, verify=False)
     return response.json()
