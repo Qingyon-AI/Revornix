eaeb1b56e5bd63be874dcf7024ca0eca16e66380
Kinda Hall <1142704468@qq.com>
2025-07-21 20:48:09 +0800

    feat: 🎸 delete the original file-backend

--- file-backend/config/sql.py
+++
@@ -1,6 +0,0 @@
-import os
-
-USER_NAME = os.environ.get('USER_NAME')
-PASSWORD = os.environ.get('PASSWORD')
-DATABASE_URL = os.environ.get('DATABASE_URL')
-DATABASE_NAME = os.environ.get('DATABASE_NAME')
--- file-backend/schemas/__init__.py
+++
@@ -1,3 +0,0 @@
-from . import file
-from . import error
-from . import common
