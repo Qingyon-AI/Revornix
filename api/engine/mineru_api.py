32567c6fe1c0ab122b345b74e67c443555041843
Kinda Hall <1142704468@qq.com>
2025-09-14 23:55:31 +0800

    fix: 🐛 fix the MineruApiEngine demo config

--- api/engine/mineru_api.py
+++ api/engine/mineru_api.py
@@ -10,4 +10,5 @@ class MineruApiEngine(EngineProtocol):
                          engine_name='MinerU API',
                          engine_name_zh='MinerU API',
                          engine_description='MinerU API is an AI-driven file parser provided by MinerU official, which can parse files such as webpages, PDFs, and images into Markdown format while retaining the original layout well.',
-                         engine_description_zh='MinerU API 是 MinerU官方提供的AI驱动的文件解析器，可以将网页、PDF、图片等文件解析为 Markdown 格式并且较好地保留原来的排版。')
+                         engine_description_zh='MinerU API 是 MinerU官方提供的AI驱动的文件解析器，可以将网页、PDF、图片等文件解析为 Markdown 格式并且较好地保留原来的排版。',
+                         engine_demo_config='{"token": "******", "uid": "******"}')
