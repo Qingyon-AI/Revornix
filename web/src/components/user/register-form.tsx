6e70e7bdb0a274094c813d082b38d8e211952db6
Kinda Hall <1142704468@qq.com>
2025-09-16 14:48:20 +0800

    feat: 🎸 optimize the register view

--- web/src/components/user/register-form.tsx
+++ web/src/components/user/register-form.tsx
@@ -5,7 +5,6 @@ import Link from 'next/link';
 import { useRouter } from 'nextjs-toploader/app';
 import Cookies from 'js-cookie';
 import {
-	Card,
 	CardContent,
 	CardDescription,
 	CardHeader,
@@ -96,23 +95,40 @@ export function RegisterForm() {
 	};
 
 	return (
-		<Card className='mx-auto max-w-sm'>
-			<Form {...form}>
-				<form onSubmit={onSubmit} className='space-y-2 min-w-80'>
-					<CardHeader className='mb-5'>
-						<CardTitle className='text-2xl'>{t('seo_register')}</CardTitle>
-						<CardDescription>{t('seo_register_description')}</CardDescription>
-					</CardHeader>
-					<CardContent>
+		<Form {...form}>
+			<form onSubmit={onSubmit} className='space-y-2 min-w-100'>
+				<CardHeader className='mb-5'>
+					<CardTitle className='text-2xl'>{t('seo_register')}</CardTitle>
+					<CardDescription>{t('seo_register_description')}</CardDescription>
+				</CardHeader>
+				<CardContent>
+					<FormField
+						control={form.control}
+						name='email'
+						render={({ field }) => (
+							<FormItem className='mb-5'>
+								<FormLabel>{t('seo_register_form_email')}</FormLabel>
+								<FormControl>
+									<Input
+										placeholder={t('seo_register_form_email_placeholder')}
+										{...field}
+									/>
+								</FormControl>
+								<FormMessage />
+							</FormItem>
+						)}
+					/>
+					<div className='grid gap-2'>
 						<FormField
 							control={form.control}
-							name='email'
+							name='password'
 							render={({ field }) => (
 								<FormItem className='mb-5'>
-									<FormLabel>{t('seo_register_form_email')}</FormLabel>
+									<FormLabel>{t('seo_register_form_password')}</FormLabel>
 									<FormControl>
 										<Input
-											placeholder={t('seo_register_form_email_placeholder')}
+											type='password'
+											placeholder={t('seo_register_form_password_placeholder')}
 											{...field}
 										/>
 									</FormControl>
@@ -120,63 +136,40 @@ export function RegisterForm() {
 								</FormItem>
 							)}
 						/>
-						<div className='grid gap-2'>
-							<FormField
-								control={form.control}
-								name='password'
-								render={({ field }) => (
-									<FormItem className='mb-5'>
-										<FormLabel>{t('seo_register_form_password')}</FormLabel>
-										<FormControl>
-											<Input
-												type='password'
-												placeholder={t(
-													'seo_register_form_password_placeholder'
-												)}
-												{...field}
-											/>
-										</FormControl>
-										<FormMessage />
-									</FormItem>
-								)}
-							/>
-						</div>
-						<div className='grid gap-2'>
-							<FormField
-								control={form.control}
-								name='passwordAgain'
-								render={({ field }) => (
-									<FormItem className='mb-5'>
-										<FormLabel>
-											{t('seo_register_form_password_again')}
-										</FormLabel>
-										<FormControl>
-											<Input
-												type='password'
-												placeholder={t(
-													'seo_register_form_password_again_placeholder'
-												)}
-												{...field}
-											/>
-										</FormControl>
-										<FormMessage />
-									</FormItem>
-								)}
-							/>
-						</div>
-						<Button className='w-full' type='submit' disabled={submitting}>
-							{t('seo_register_submit')}
-							{submitting && <Loader2 className='mr-1 size-4 animate-spin' />}
-						</Button>
-						<div className='mt-4 text-center text-sm'>
-							{t('seo_register_already_have_account')}
-							<Link href='/login' className='underline'>
-								{t('seo_login_go_to_login')}
-							</Link>
-						</div>
-					</CardContent>
-				</form>
-			</Form>
-		</Card>
+					</div>
+					<div className='grid gap-2'>
+						<FormField
+							control={form.control}
+							name='passwordAgain'
+							render={({ field }) => (
+								<FormItem className='mb-5'>
+									<FormLabel>{t('seo_register_form_password_again')}</FormLabel>
+									<FormControl>
+										<Input
+											type='password'
+											placeholder={t(
+												'seo_register_form_password_again_placeholder'
+											)}
+											{...field}
+										/>
+									</FormControl>
+									<FormMessage />
+								</FormItem>
+							)}
+						/>
+					</div>
+					<Button className='w-full' type='submit' disabled={submitting}>
+						{t('seo_register_submit')}
+						{submitting && <Loader2 className='mr-1 size-4 animate-spin' />}
+					</Button>
+					<div className='mt-4 text-center text-sm'>
+						<span className='mr-2'>{t('seo_register_already_have_account')}</span>
+						<Link href='/login' className='underline'>
+							{t('seo_login_go_to_login')}
+						</Link>
+					</div>
+				</CardContent>
+			</form>
+		</Form>
 	);
 }
