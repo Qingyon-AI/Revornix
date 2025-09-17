4a124a36ddca4bf8fa77e55a2bc7b0f7fe9b36cf
Kinda Hall <1142704468@qq.com>
2025-09-17 19:48:23 +0800

    fix: 🐛 fix the section detail page

--- web/src/components/section/section-container.tsx
+++ web/src/components/section/section-container.tsx
@@ -1,27 +1,17 @@
 'use client';
 
-import { getSectionDetail } from '@/service/section';
-import { useQuery } from '@tanstack/react-query';
 import SectionInfo from './section-info';
 import SectionMarkdown from './section-markdown';
 
 const SectionContainer = ({ id }: { id: number }) => {
-	const { data: section } = useQuery({
-		queryKey: ['getSectionDetail', id],
-		queryFn: async () => {
-			return getSectionDetail({ section_id: id });
-		},
-	});
 	return (
 		<div className='px-5 pb-5 h-full w-full flex flex-row relative overflow-auto'>
-			{section && (
-				<div className='grid grid-cols-12 gap-5 w-full'>
-					<div className='flex-1 h-full col-span-8 overflow-auto'>
-						<SectionMarkdown id={id} />
-					</div>
-					<SectionInfo id={id} />
+			<div className='grid grid-cols-12 gap-5 w-full'>
+				<div className='flex-1 h-full col-span-8 overflow-auto'>
+					<SectionMarkdown id={id} />
 				</div>
-			)}
+				<SectionInfo id={id} />
+			</div>
 		</div>
 	);
 };
--- web/src/components/section/section-info.tsx
+++ web/src/components/section/section-info.tsx
@@ -4,23 +4,24 @@ import { getSectionDetail } from '@/service/section';
 import { useQuery } from '@tanstack/react-query';
 import { formatDistance } from 'date-fns';
 import { Card } from '../ui/card';
-import { useUserContext } from '@/provider/user-provider';
 import SectionDocument from './section-document';
 import { useRouter } from 'nextjs-toploader/app';
 import SectionComments from './section-comments';
 import { Badge } from '../ui/badge';
 import { useLocale, useTranslations } from 'next-intl';
 import CustomImage from '../ui/custom-image';
-import SectionOperate from './section-operate';
-import { Alert, AlertTitle } from '../ui/alert';
-import { ShieldAlert } from 'lucide-react';
+import { Skeleton } from '../ui/skeleton';
 
 const SectionInfo = ({ id }: { id: number }) => {
 	const locale = useLocale();
 	const t = useTranslations();
 	const router = useRouter();
 
-	const { data: section } = useQuery({
+	const {
+		data: section,
+		isFetching,
+		isFetched,
+	} = useQuery({
 		queryKey: ['getSectionDetail', id],
 		queryFn: async () => {
 			return getSectionDetail({ section_id: id });
@@ -37,54 +38,66 @@ const SectionInfo = ({ id }: { id: number }) => {
 						className='w-full h-64 object-cover'
 					/>
 				</div>
-				{section?.update_time && (
+
+				{isFetching && !isFetched && (
 					<div className='px-5 mb-3'>
-						<p className='text-xs text-muted-foreground'>
-							{t('section_updated_at')}{' '}
-							{formatDistance(new Date(section.update_time), new Date(), {
-								addSuffix: true,
-								locale: locale === 'zh' ? zhCN : enUS,
-							})}
-						</p>
+						<Skeleton className='w-full h-40' />
 					</div>
 				)}
-				<div className='flex flex-row justify-between items-center px-5 mb-3'>
-					<div className='font-bold text-lg'>
-						{section?.title ? section?.title : t('section_title_empty')}
-					</div>
-				</div>
-				<div className='text-sm text-muted-foreground mb-3 px-5'>
-					{section?.description
-						? section?.description
-						: t('section_description_empty')}
-				</div>
-				{section?.labels && section.labels.length > 0 && (
-					<div className='flex flex-row gap-2 items-center px-5 mb-3 flex-wrap'>
-						{section.labels.map((label) => {
-							return (
-								<Badge key={label.id} variant={'secondary'}>
-									{label.name}
-								</Badge>
-							);
-						})}
-					</div>
+
+				{isFetched && (
+					<>
+						{section?.update_time && (
+							<div className='px-5 mb-3'>
+								<p className='text-xs text-muted-foreground'>
+									{t('section_updated_at')}{' '}
+									{formatDistance(new Date(section.update_time), new Date(), {
+										addSuffix: true,
+										locale: locale === 'zh' ? zhCN : enUS,
+									})}
+								</p>
+							</div>
+						)}
+						<div className='flex flex-row justify-between items-center px-5 mb-3'>
+							<div className='font-bold text-lg'>
+								{section?.title ? section?.title : t('section_title_empty')}
+							</div>
+						</div>
+						<div className='text-sm text-muted-foreground mb-3 px-5'>
+							{section?.description
+								? section?.description
+								: t('section_description_empty')}
+						</div>
+						{section?.labels && section.labels.length > 0 && (
+							<div className='flex flex-row gap-2 items-center px-5 mb-3 flex-wrap'>
+								{section.labels.map((label) => {
+									return (
+										<Badge key={label.id} variant={'secondary'}>
+											{label.name}
+										</Badge>
+									);
+								})}
+							</div>
+						)}
+						<div
+							className='flex flex-row items-center gap-1 px-5 mb-3'
+							onClick={(e) => {
+								router.push(`/user/detail/${section?.creator.id}`);
+								e.preventDefault();
+								e.stopPropagation();
+							}}>
+							<CustomImage
+								src={section?.creator.avatar!}
+								alt='avatar'
+								className='rounded-full object-cover w-5 h-5'
+							/>
+							<p className='text-xs text-muted-foreground'>
+								{section?.creator.nickname}
+							</p>
+						</div>
+					</>
 				)}
-				<div
-					className='flex flex-row items-center gap-1 px-5 mb-3'
-					onClick={(e) => {
-						router.push(`/user/detail/${section?.creator.id}`);
-						e.preventDefault();
-						e.stopPropagation();
-					}}>
-					<CustomImage
-						src={section?.creator.avatar!}
-						alt='avatar'
-						className='rounded-full object-cover w-5 h-5'
-					/>
-					<p className='text-xs text-muted-foreground'>
-						{section?.creator.nickname}
-					</p>
-				</div>
+				
 				<div className='px-5 mb-3'>
 					<SectionDocument id={id} />
 				</div>
