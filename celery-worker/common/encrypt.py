f52085a127579b2d1fdbf8d5248e765be3eb83be
Kinda Hall <1142704468@qq.com>
2025-09-19 22:30:47 +0800

    feat: 🎸 update the env

--- celery-worker/common/encrypt.py
+++ celery-worker/common/encrypt.py
@@ -2,7 +2,7 @@ from cryptography.hazmat.primitives.ciphers.aead import AESGCM
 import os, base64
 
 # 系统环境变量里的主密钥
-MASTER_KEY = base64.b64decode(os.getenv("APIKEY_ENCRYPT_KEY", "623f8bce26d03a8f93b2a9ba9fbbaa5b"))
+MASTER_KEY = base64.b64decode(os.getenv("APIKEY_ENCRYPT_KEY"))
 
 def encrypt_api_key(api_key: str) -> str:
     aesgcm = AESGCM(MASTER_KEY)
