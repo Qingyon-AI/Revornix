60e53351803586887f86930f7dd12ea3eec01d32
Kinda Hall <1142704468@qq.com>
2025-09-29 21:18:35 +0800

    feat: 🎸 update neo4j plugins

---
+++ celery-worker/protocol/tts_engine.py
@@ -0,0 +1,8 @@
+class TTSEngine():
+    def __init__(self, app_id, api_key, api_secret):
+        ...
+
+    def synthesize(self, text, voice_name, speed, volume, pitch):
+        # 调用火山引擎的API进行语音合成
+        # 返回合成的音频文件
+        ...
