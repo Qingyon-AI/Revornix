b68d98ed9e7d28b74eabfc22058a1bd2621201c8
Kinda Hall <1142704468@qq.com>
2025-10-25 18:20:30 +0800

    feat: 🎸 add oidc server

---
+++ oidc/schemas/oauth.py
@@ -0,0 +1,15 @@
+from pydantic import BaseModel
+
+class OAuthClientAddRequest(BaseModel):
+    name: str
+    description: str | None = None
+    redirect_uris: list[str] = []
+    
+class OAuthClientDeleteRequest(BaseModel):
+    client_id: str
+    
+class OAuthClientUpdateRequest(BaseModel):
+    client_id: str
+    name: str | None = None
+    description: str | None = None
+    redirect_uris: list[str] | None = None
