a8aae37558c0567c5bb7f6c003aeb29d21c977a1
Kinda Hall <1142704468@qq.com>
2025-11-01 14:26:34 +0800

    feat: 🎸 catch wechat utils error

--- api/common/tp_auth/wechat_utils.py
+++ api/common/tp_auth/wechat_utils.py
@@ -1,5 +1,6 @@
 import httpx
 from pydantic import BaseModel
+from common.logger import exception_logger
 
 class WebWeChatTokenResponse(BaseModel):
     access_token: str
@@ -20,14 +21,22 @@ def get_mini_wechat_tokens(app_id: str,
                            app_secret: str, 
                            code: str):
     response_token = httpx.post(f'https://api.weixin.qq.com/sns/jscode2session?appid={app_id}&secret={app_secret}&js_code={code}&grant_type=authorization_code')
-    res = MiniWeChatTokenResponse(**response_token.json())
+    try:
+        res = MiniWeChatTokenResponse(**response_token.json())
+    except Exception as e:
+        exception_logger.error(f'get_mini_wechat_tokens error: {response_token.json()}')
+        raise e
     return res
 
 def get_web_wechat_tokens(app_id: str, 
                           app_secret: str, 
                           code: str):
     response_token = httpx.post(f'https://api.weixin.qq.com/sns/oauth2/access_token?appid={app_id}&secret={app_secret}&code={code}&grant_type=authorization_code')
-    res = WebWeChatTokenResponse(**response_token.json())
+    try:
+        res = WebWeChatTokenResponse(**response_token.json())
+    except Exception as e:
+        exception_logger.error(f'get_web_wechat_tokens error: {response_token.json()}')
+        raise e
     return res
 
 def get_web_user_info(access_token: str,
