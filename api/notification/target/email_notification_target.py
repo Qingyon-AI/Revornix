ad71e588b614a552c9f22227f600a26cad081a0b
Kinda Hall <1142704468@qq.com>
2025-11-23 15:10:02 +0800

    fix: 🐛 fix the notification target

---
+++ api/notification/target/email_notification_target.py
@@ -0,0 +1,18 @@
+import json
+from protocol.notification_target import NotificationTargetProtocol
+
+class EmailNotificationTarget(NotificationTargetProtocol):
+    
+    def __init__(
+        self
+    ):
+        super().__init__(
+            uuid='341ff369ce32418abb8b4f12ac607059',
+            name='Email Target',
+            name_zh='邮件目标',
+            description='The notification target which can be sent email',
+            description_zh='可以被邮件服务器发送邮件的消息目标',
+            demo_config=json.dumps({
+                'email': 'your_email@example.com',
+            }) 
+        )
