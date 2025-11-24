dc4e0ca90b799888296ced8d738e1c8211becca6
Kinda Hall <1142704468@qq.com>
2025-11-24 16:39:34 +0800

    refactor: 💡 Update the section_commented trigger description

--- celery-worker/notification/trigger_event/section_commented.py
+++ celery-worker/notification/trigger_event/section_commented.py
@@ -9,6 +9,6 @@ class SectionCommentedNotificationTriggerEvent(NotificationTriggerEventProtocol)
             uuid='2840a3b104124bd59afbae2a57c93dbf',
             name = "SectionCommented",
             name_zh = "专栏被评论",
-            description = "When the section which you subscribe/participate has been commented",
-            description_zh = "当你订阅/参与的专栏被评论的时候"
+            description = "When the section which you participate has been commented",
+            description_zh = "当你参与的专栏被评论的时候"
         )
