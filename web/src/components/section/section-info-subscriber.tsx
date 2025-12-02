8c4d9543026ada18708856b22a54251b07813d71
Kinda Hall <1142704468@qq.com>
2025-12-02 14:41:25 +0800

    feat: 🎸 Optimize the UI of the section info page

--- web/src/components/section/section-info-subscriber.tsx
+++ web/src/components/section/section-info-subscriber.tsx
@@ -84,89 +84,104 @@ const SectionInfoSubscriber = ({ section_id }: { section_id: number }) => {
 	return (
 		<div>
 			<div className='flex flex-wrap -space-x-1 *:data-[slot=avatar]:ring-background *:data-[slot=avatar]:ring-2 *:data-[slot=avatar]:grayscale'>
-				{users.slice(0, 40).map((user, index) => {
-					return (
-						<Avatar
-							key={index}
-							className='size-6'
-							title={user.nickname ? user.nickname : 'Unknown User'}
-							onClick={(e) => {
-								router.push(`/user/detail/${user.id}`);
-								e.preventDefault();
-								e.stopPropagation();
-							}}>
-							<AvatarImage
-								src={user.avatar}
-								alt='user avatar'
-								className='size-6'
-							/>
-							<AvatarFallback className='size-6'>
-								{user.nickname}
-							</AvatarFallback>
-						</Avatar>
-					);
-				})}
-				<Sheet>
-					<SheetTrigger asChild>
-						<div className='size-6 z-10 rounded-full bg-muted flex justify-center items-center hover:cursor-pointer'>
-							<Ellipsis />
-						</div>
-					</SheetTrigger>
-					<SheetContent>
-						<SheetHeader>
-							<SheetTitle>{t('section_subscribers')}</SheetTitle>
-							<Input
-								className='w-full'
-								placeholder={t('section_subscribers_search')}
-								value={keyword}
-								onChange={(e) => setKeyword(e.target.value)}
-							/>
-						</SheetHeader>
-						{!isFetching && users && users.length === 0 && (
-							<div className='flex-1 flex justify-center items-center text-xs text-muted-foreground'>
-								{t('section_subscribers_empty')}
-							</div>
-						)}
-						<div className='px-5 flex-1 overflow-auto text-xs text-muted-foreground gap-3 flex flex-col'>
-							{users.map((user, index) => {
-								const isLast = index === users.length - 1;
-								return (
-									<div
-										ref={isLast ? loadMoreRef : null}
-										key={index}
-										className='bg-muted/50 flex flex-row items-center gap-5 rounded-lg p-3'
-										onClick={(e) => {
-											router.push(`/user/detail/${user.id}`);
-											e.preventDefault();
-											e.stopPropagation();
-										}}>
-										<Avatar
-											key={index}
-											title={user.nickname ? user.nickname : 'Unknown User'}>
-											<AvatarImage src={user.avatar} alt='user avatar' />
-											<AvatarFallback>{user.nickname}</AvatarFallback>
-										</Avatar>
-										<div className='flex flex-col'>
-											<div className='mb-1'>{user.nickname}</div>
-											<div>{user.slogan}</div>
-										</div>
+				{!isFetching && users && users.length === 0 && (
+					<div className='text-xs text-muted-foreground'>
+						{t('section_subscribers_empty')}
+					</div>
+				)}
+				{!isFetching && users && users.length > 0 && (
+					<>
+						{users.slice(0, 40).map((user, index) => {
+							return (
+								<Avatar
+									key={index}
+									className='size-6'
+									title={user.nickname ? user.nickname : 'Unknown User'}
+									onClick={(e) => {
+										router.push(`/user/detail/${user.id}`);
+										e.preventDefault();
+										e.stopPropagation();
+									}}>
+									<AvatarImage
+										src={user.avatar}
+										alt='user avatar'
+										className='size-6'
+									/>
+									<AvatarFallback className='size-6'>
+										{user.nickname}
+									</AvatarFallback>
+								</Avatar>
+							);
+						})}
+						<Sheet>
+							<SheetTrigger asChild>
+								<div className='size-6 z-10 rounded-full bg-muted flex justify-center items-center hover:cursor-pointer'>
+									<Ellipsis />
+								</div>
+							</SheetTrigger>
+							<SheetContent>
+								<SheetHeader>
+									<SheetTitle>{t('section_subscribers')}</SheetTitle>
+									<Input
+										className='w-full'
+										placeholder={t('section_subscribers_search')}
+										value={keyword}
+										onChange={(e) => setKeyword(e.target.value)}
+									/>
+								</SheetHeader>
+								{!isFetching && users && users.length === 0 && (
+									<div className='flex-1 flex justify-center items-center text-xs text-muted-foreground'>
+										{t('section_subscribers_empty')}
 									</div>
-								);
-							})}
-							{isFetching &&
-								!userPages &&
-								[...Array(20)].map((number, index) => {
-									return <Skeleton className='w-full h-12 mb-3' key={index} />;
-								})}
-							{/* ✅ 底部加载状态 */}
-							{isFetchingNextPage &&
-								userPages &&
-								[...Array(20)].map((number, index) => {
-									return <Skeleton className='w-full h-12 mb-3' key={index} />;
-								})}
-						</div>
-					</SheetContent>
-				</Sheet>
+								)}
+								<div className='px-5 flex-1 overflow-auto text-xs text-muted-foreground gap-3 flex flex-col'>
+									{users.map((user, index) => {
+										const isLast = index === users.length - 1;
+										return (
+											<div
+												ref={isLast ? loadMoreRef : null}
+												key={index}
+												className='bg-muted/50 flex flex-row items-center gap-5 rounded-lg p-3'
+												onClick={(e) => {
+													router.push(`/user/detail/${user.id}`);
+													e.preventDefault();
+													e.stopPropagation();
+												}}>
+												<Avatar
+													key={index}
+													title={
+														user.nickname ? user.nickname : 'Unknown User'
+													}>
+													<AvatarImage src={user.avatar} alt='user avatar' />
+													<AvatarFallback>{user.nickname}</AvatarFallback>
+												</Avatar>
+												<div className='flex flex-col'>
+													<div className='mb-1'>{user.nickname}</div>
+													<div>{user.slogan}</div>
+												</div>
+											</div>
+										);
+									})}
+									{isFetching &&
+										!userPages &&
+										[...Array(20)].map((number, index) => {
+											return (
+												<Skeleton className='w-full h-12 mb-3' key={index} />
+											);
+										})}
+									{/* ✅ 底部加载状态 */}
+									{isFetchingNextPage &&
+										userPages &&
+										[...Array(20)].map((number, index) => {
+											return (
+												<Skeleton className='w-full h-12 mb-3' key={index} />
+											);
+										})}
+								</div>
+							</SheetContent>
+						</Sheet>
+					</>
+				)}
 			</div>
 		</div>
 	);
