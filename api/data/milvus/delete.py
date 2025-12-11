f04e74337633a33e05938ccb8cfdc705c0fab88c
Kinda Hall <1142704468@qq.com>
2025-12-11 21:08:38 +0800

    feat: 🎸 Delete the init_vector_base_data.py, use the create

--- api/data/milvus/delete.py
+++ api/data/milvus/delete.py
@@ -1,6 +1,6 @@
 from data.milvus.base import milvus_client, MILVUS_COLLECTION
 
 def clear_milvus_collection():
-    if MILVUS_COLLECTION in milvus_client.list_collections():
+    if milvus_client.has_collection(MILVUS_COLLECTION): 
         milvus_client.drop_collection(MILVUS_COLLECTION)
         print(f"Milvus collection {MILVUS_COLLECTION} dropped.")
