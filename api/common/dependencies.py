b3fe93cfb56f47e0c8e1b3b07d0a01505ac6f522
Kinda Hall <1142704468@qq.com>
2025-12-15 16:25:55 +0800

    feat: 🎸 Optimize dependencies

--- api/common/dependencies.py
+++ api/common/dependencies.py
@@ -8,22 +8,22 @@ from data.sql.base import SessionLocal
 from datetime import datetime, timezone
 from config.oauth2 import OAUTH_SECRET_KEY
 from config.base import OFFICIAL, DEPLOY_HOSTS
+from urllib.parse import urlparse
 from fastapi import Request, HTTPException, status, Depends, Header
 
 if OAUTH_SECRET_KEY is None:
     raise Exception("OAUTH_SECRET_KEY is not set")
+    
+async def get_request_host(request: Request) -> str | None:
+    origin = request.headers.get("origin")
+    if origin:
+        return urlparse(origin).netloc
 
-async def get_request_host(request: Request) -> str:
-    host = (
-        request.headers.get("x-forwarded-host")
-        or request.headers.get("host")
-        or request.url.hostname
-    )
+    referer = request.headers.get("referer")
+    if referer:
+        return urlparse(referer).netloc
 
-    if host is None:
-        raise ValueError("Host not found in request")
-
-    return host
+    return None
 
 async def check_deployed_by_official(
     host = Depends(get_request_host)
@@ -52,28 +52,41 @@ def get_db():
     finally:
         db.close()
 
-def decode_jwt_token(token: str, secret_key: str = OAUTH_SECRET_KEY):
+def decode_jwt_token(
+    token: str, 
+    secret_key: str = OAUTH_SECRET_KEY
+):
     return jwt.decode(token, secret_key, algorithms=["HS256"])
 
 def get_cache(request: Request) -> Redis:
     return request.app.state.redis
 
-def get_api_key(api_key: str | None = Header(default=None), 
-                db: Session = Depends(get_db)) -> models.api_key.ApiKey:
-    db_api_key = crud.api_key.get_api_key_by_api_key(db=db, 
-                                                     api_key=api_key)
+def get_api_key(
+    api_key: str | None = Header(default=None), 
+    db: Session = Depends(get_db)
+):
+    db_api_key = crud.api_key.get_api_key_by_api_key(
+        db=db, 
+        api_key=api_key
+    )
     if db_api_key is None:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid API Key")
     return db_api_key
 
-def get_current_user_with_api_key(api_key: models.api_key.ApiKey = Depends(get_api_key),
-                                  db: Session = Depends(get_db)) -> models.user.User:
-     user = crud.user.get_user_by_id(db=db, 
-                                     user_id=api_key.user_id)
+def get_current_user_with_api_key(
+    api_key: models.api_key.ApiKey = Depends(get_api_key),
+    db: Session = Depends(get_db)
+):
+     user = crud.user.get_user_by_id(
+         db=db, 
+         user_id=api_key.user_id
+        )
      return user
 
-def get_real_ip(request: Request, 
-                x_forwarded_for: str | None = Header(default=None)) -> str:
+def get_real_ip(
+    request: Request, 
+    x_forwarded_for: str | None = Header(default=None)
+) -> str:
     if x_forwarded_for:
         # 取第一个IP，因为X-Forwarded-For可能包含多个IP，由逗号分隔
         ip = x_forwarded_for.split(",")[0]
