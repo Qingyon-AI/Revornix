ec745fc69b4e30cc53756699f736414ca296a63e
Kinda Hall <1142704468@qq.com>
2026-01-09 20:17:31 +0800

    fix: 🐛 Prevent tooltip clicks from submitting parent forms

--- web/src/components/ui/hybrid-tooltip.tsx
+++ web/src/components/ui/hybrid-tooltip.tsx
@@ -1,82 +1,116 @@
-'use client'
+'use client';
 
-import { PopoverContentProps, PopoverProps, PopoverTriggerProps } from '@radix-ui/react-popover'
-import { TooltipContentProps, TooltipProps, TooltipTriggerProps, TooltipProviderProps } from '@radix-ui/react-tooltip'
-import { throttle } from 'lodash-es'
-import { createContext, useCallback, useContext, useEffect, useState } from 'react'
-
-import { Popover, PopoverTrigger, PopoverContent } from './popover'
 import {
-  TooltipProvider as OriginalTooltipProvider,
-  Tooltip as OriginalTooltip,
-  TooltipTrigger as OriginalTooltipTrigger,
-  TooltipContent as OriginalTooltipContent
-} from './tooltip'
+	PopoverContentProps,
+	PopoverProps,
+	PopoverTriggerProps,
+} from '@radix-ui/react-popover';
+import {
+	TooltipContentProps,
+	TooltipProps,
+	TooltipTriggerProps,
+	TooltipProviderProps,
+} from '@radix-ui/react-tooltip';
+import { throttle } from 'lodash-es';
+import {
+	createContext,
+	useCallback,
+	useContext,
+	useEffect,
+	useState,
+} from 'react';
 
-const TouchContext = createContext<boolean | undefined>(undefined)
-const useTouch = () => useContext(TouchContext)
+import { Popover, PopoverTrigger, PopoverContent } from './popover';
+import {
+	TooltipProvider as OriginalTooltipProvider,
+	Tooltip as OriginalTooltip,
+	TooltipTrigger as OriginalTooltipTrigger,
+	TooltipContent as OriginalTooltipContent,
+} from './tooltip';
 
-export const TooltipProvider = ({ children, ...props }: TooltipProviderProps) => {
-  const [isTouch, setTouch] = useState<boolean | undefined>(undefined)
+const TouchContext = createContext<boolean | undefined>(undefined);
+const useTouch = () => useContext(TouchContext);
 
-  useEffect(() => {
-    setTouch(window.matchMedia('(pointer: coarse)').matches)
-  }, [])
+export const TooltipProvider = ({
+	children,
+	...props
+}: TooltipProviderProps) => {
+	const [isTouch, setTouch] = useState<boolean | undefined>(undefined);
 
-  return (
-    <TouchContext.Provider value={isTouch}>
-      <OriginalTooltipProvider {...props}>{children}</OriginalTooltipProvider>
-    </TouchContext.Provider>
-  )
-}
+	useEffect(() => {
+		setTouch(window.matchMedia('(pointer: coarse)').matches);
+	}, []);
 
-export const Tooltip = (props: TooltipProps & PopoverProps & { trackingRef?: React.RefObject<HTMLElement> }) => {
-  const isTouch = useTouch()
-  const { trackingRef } = props
+	return (
+		<TouchContext.Provider value={isTouch}>
+			<OriginalTooltipProvider {...props}>{children}</OriginalTooltipProvider>
+		</TouchContext.Provider>
+	);
+};
 
-  const [open, setOpen] = useState(false)
+export const Tooltip = (
+	props: TooltipProps &
+		PopoverProps & { trackingRef?: React.RefObject<HTMLElement> }
+) => {
+	const isTouch = useTouch();
+	const { trackingRef } = props;
 
-  const closeToolTip = useCallback(
-    () =>
-      throttle(() => {
-        setOpen((currentOpen) => {
-          if (!currentOpen || !isTouch) return currentOpen
-          return false
-        })
-      }, 500),
-    [isTouch]
-  )
+	const [open, setOpen] = useState(false);
 
-  useEffect(() => {
-    const targetDom = trackingRef?.current ?? window
-    targetDom.addEventListener('scroll', closeToolTip)
-    return () => {
-      targetDom.removeEventListener('scroll', closeToolTip)
-    }
-  }, [closeToolTip, trackingRef])
+	const closeToolTip = useCallback(
+		() =>
+			throttle(() => {
+				setOpen((currentOpen) => {
+					if (!currentOpen || !isTouch) return currentOpen;
+					return false;
+				});
+			}, 500),
+		[isTouch]
+	);
 
-  return isTouch ? (
-    <Popover open={open} onOpenChange={setOpen} {...props} />
-  ) : (
-    <OriginalTooltip delayDuration={0} {...props} />
-  )
-}
+	useEffect(() => {
+		const targetDom = trackingRef?.current ?? window;
+		targetDom.addEventListener('scroll', closeToolTip);
+		return () => {
+			targetDom.removeEventListener('scroll', closeToolTip);
+		};
+	}, [closeToolTip, trackingRef]);
 
-export const TooltipTrigger = (props: TooltipTriggerProps & PopoverTriggerProps) => {
-  const isTouch = useTouch()
+	return isTouch ? (
+		<Popover open={open} onOpenChange={setOpen} {...props} />
+	) : (
+		<OriginalTooltip delayDuration={0} {...props} />
+	);
+};
 
-  return isTouch ? <PopoverTrigger {...props} /> : <OriginalTooltipTrigger {...props} />
-}
+export const TooltipTrigger = (
+	props: TooltipTriggerProps & PopoverTriggerProps
+) => {
+	const isTouch = useTouch();
 
-export const TooltipContent = (props: TooltipContentProps & PopoverContentProps) => {
-  const isTouch = useTouch()
+	const triggerProps = {
+		...props,
+		type: 'button' as const, // ⭐ 关键
+	};
 
-  return isTouch ? (
-    <PopoverContent
-      className="z-50 w-fit overflow-hidden rounded-md bg-primary px-3 py-1.5 text-xs text-primary animate-in fade-in-0 zoom-in-95"
-      {...props}
-    />
-  ) : (
-    <OriginalTooltipContent {...props} />
-  )
-}
+	return isTouch ? (
+		<PopoverTrigger {...triggerProps} />
+	) : (
+		<OriginalTooltipTrigger {...triggerProps} />
+	);
+};
+
+export const TooltipContent = (
+	props: TooltipContentProps & PopoverContentProps
+) => {
+	const isTouch = useTouch();
+
+	return isTouch ? (
+		<PopoverContent
+			className='z-50 w-fit overflow-hidden rounded-md bg-primary px-3 py-1.5 text-xs text-primary animate-in fade-in-0 zoom-in-95'
+			{...props}
+		/>
+	) : (
+		<OriginalTooltipContent {...props} />
+	);
+};
