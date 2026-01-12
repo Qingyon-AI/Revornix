46f83412fa636aee42697bcd2e2b6de576eed0a0
Kinda Hall <1142704468@qq.com>
2026-01-12 21:18:34 +0800

    feat: 🎸 Optimize the UI of the section documents

--- web/src/components/section/section-document-card.tsx
+++ web/src/components/section/section-document-card.tsx
@@ -20,68 +20,68 @@ const SectionDocumentCard = ({
 	return (
 		<div
 			onClick={() => router.push(`/document/detail/${document.id}`)}
-			className='relative bg-white dark:bg-black rounded ring-1 ring-inset dark:ring-white/10 ring-black/10 flex justify-between items-center gap-3 p-5'>
-			<div className='flex flex-col gap-2'>
-				<div className='text-sm font-bold line-clamp-1'>
-					{document.title
-						? document.title
-						: t('section_document_card_no_title')}
-				</div>
-				<div className='text-xs text-muted-foreground line-clamp-2 break-all'>
-					{document.description
-						? document.description
-						: t('section_document_card_no_description')}
-				</div>
-				<div className='flex flex-row items-center gap-2 overflow-auto'>
-					<div className='w-fit text-xs text-muted-foreground px-2 py-1 rounded bg-muted'>
-						{t('document_category') + ': '}
-						{document.category === DocumentCategory.WEBSITE
-							? t('document_category_link')
-							: document.category === DocumentCategory.FILE
-							? t('document_category_file')
-							: document.category === DocumentCategory.QUICK_NOTE
-							? t('document_category_quick_note')
-							: t('document_category_others')}
+			className='relative bg-white dark:bg-black rounded ring-1 ring-inset dark:ring-white/10 ring-black/10 p-5'>
+			<div className='flex flex-row mb-3'>
+				<div className='flex-1'>
+					<div className='text-sm font-bold line-clamp-1'>
+						{document.title
+							? document.title
+							: t('section_document_card_no_title')}
 					</div>
-					<div className='w-fit text-xs text-muted-foreground px-2 py-1 rounded bg-muted'>
-						{t('section_document_card_section_supplement') + ': '}
-						{document.status === SectionDocumentIntegration.WAIT_TO
-							? t('section_document_card_section_supplement_todo')
-							: document.status === SectionDocumentIntegration.SUPPLEMENTING
-							? t('section_document_card_section_supplement_doing')
-							: document.status === SectionDocumentIntegration.SUCCESS
-							? t('section_document_card_section_supplement_done')
-							: document.status === SectionDocumentIntegration.FAILED
-							? t('section_document_card_section_supplement_failed')
-							: t('section_document_card_section_supplement_unknown')}
+					<div className='text-xs text-muted-foreground line-clamp-2 break-all'>
+						{document.description
+							? document.description
+							: t('section_document_card_no_description')}
 					</div>
 				</div>
-
-				{document.labels && document.labels.length > 0 && (
-					<div className='flex flex-row gap-3 items-center'>
-						{document.labels?.map((label, index) => {
-							return (
-								<div
-									key={index}
-									className='w-fit text-xs text-muted-foreground px-2 py-1 rounded bg-muted'>
-									{label.name}
-								</div>
-							);
-						})}
-					</div>
+				{document.cover && (
+					<img
+						src={document.cover}
+						alt='cover'
+						className='relative h-16 aspect-square rounded overflow-hidden shrink-0 object-cover'
+					/>
 				)}
-				<div className='text-xs text-muted-foreground'>
-					{document.create_time &&
-						format(new Date(document.create_time), 'yyyy-MM-dd HH:mm:ss')}
+			</div>
+			<div className='flex flex-row items-center gap-2 overflow-auto mb-3'>
+				<div className='w-fit text-xs text-muted-foreground px-2 py-1 rounded bg-muted'>
+					{document.category === DocumentCategory.WEBSITE
+						? t('document_category_link')
+						: document.category === DocumentCategory.FILE
+						? t('document_category_file')
+						: document.category === DocumentCategory.QUICK_NOTE
+						? t('document_category_quick_note')
+						: t('document_category_others')}
+				</div>
+				<div className='w-fit text-xs text-muted-foreground px-2 py-1 rounded bg-muted'>
+					{document.status === SectionDocumentIntegration.WAIT_TO
+						? t('section_document_card_section_supplement_todo')
+						: document.status === SectionDocumentIntegration.SUPPLEMENTING
+						? t('section_document_card_section_supplement_doing')
+						: document.status === SectionDocumentIntegration.SUCCESS
+						? t('section_document_card_section_supplement_done')
+						: document.status === SectionDocumentIntegration.FAILED
+						? t('section_document_card_section_supplement_failed')
+						: t('section_document_card_section_supplement_unknown')}
 				</div>
 			</div>
-			{document.cover && (
-				<img
-					src={document.cover}
-					alt='cover'
-					className='relative h-16 aspect-square rounded overflow-hidden shrink-0 object-cover'
-				/>
+
+			{document.labels && document.labels.length > 0 && (
+				<div className='flex flex-row gap-3 items-center'>
+					{document.labels?.map((label, index) => {
+						return (
+							<div
+								key={index}
+								className='w-fit text-xs text-muted-foreground px-2 py-1 rounded bg-muted'>
+								{label.name}
+							</div>
+						);
+					})}
+				</div>
 			)}
+			<div className='text-xs text-muted-foreground'>
+				{document.create_time &&
+					format(new Date(document.create_time), 'yyyy-MM-dd HH:mm:ss')}
+			</div>
 		</div>
 	);
 };
