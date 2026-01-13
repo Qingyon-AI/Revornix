3344a62068d46e86a4201c85e182352ee8ba4186
Kinda Hall <1142704468@qq.com>
2026-01-13 21:20:38 +0800

    refactor: 💡 Remove the oidc service

--- oidc/common/redis.py
+++
@@ -1,9 +0,0 @@
-import os
-from redis.asyncio import Redis
-
-async def redis_pool() -> Redis:
-    return await Redis(host=f"redis://{os.environ.get('REDIS_URL')}", 
-                       port=int(os.environ.get('REDIS_PORT')), 
-                       db=1, 
-                       encoding="utf-8", 
-                       decode_responses=True)
--- oidc/config/oauth2.py
+++
@@ -1,6 +0,0 @@
-from pathlib import Path
-from config.base import BASE_DIR
-
-ISSUER: str = "http://localhost:8002"   # 你的 OP 根地址（外部可访问）
-JWK_PUBLIC_PATH: Path = BASE_DIR / 'keys' / 'public.pem'
-JWK_PRIVATE_PATH: Path = BASE_DIR / 'keys' / 'private.pem'
