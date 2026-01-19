c1d8dbb88feac3d6c7775983f91ccde4c6cdcd9b
Kinda Hall <1142704468@qq.com>
2026-01-19 15:49:54 +0800

    feat: 🎸 Refactor the engine-related api

--- api/schemas/engine.py
+++ api/schemas/engine.py
@@ -2,26 +2,9 @@ from datetime import datetime, timezone
 
 from pydantic import BaseModel, field_serializer
 
+from .user import UserPublicInfo
 
-class EngineDeleteRequest(BaseModel):
-    user_engine_id: int
-
-class EngineInstallRequest(BaseModel):
-    engine_id: int
-    title: str
-    description: str | None = None
-    config_json: str | None = None
-
-class EngineInstallResponse(BaseModel):
-    user_engine_id: int
-
-class EngineUpdateRequest(BaseModel):
-    user_engine_id: int
-    config_json: str| None = None
-    title: str | None = None
-    description: str | None = None
-
-class EngineInfo(BaseModel):
+class EngineProvidedInfo(BaseModel):
     id: int
     category: int
     name: str
@@ -32,40 +15,136 @@ class EngineInfo(BaseModel):
     class Config:
         from_attributes = True
 
-class UserEngineInfo(BaseModel):
-    id: int
-    engine_id: int
-    category: int
-    title: str
+class EngineProvidedSearchRequest(BaseModel):
+    keyword: str
+    filter_category: int | None = None
+
+class EngineProvidedSearchResponse(BaseModel):
+    data: list[EngineProvidedInfo]
+    class Config:
+        from_attributes = True
+
+class EngineCreateRequest(BaseModel):
+    name: str
     description: str | None = None
-    demo_config: str | None = None
-    enable: bool | None = None
+    is_public: bool
+    engine_provided_id: int
     config_json: str | None = None
+
+class EngineDetailRequest(BaseModel):
+    engine_id: int
+
+class EngineDetail(BaseModel):
+    id: int
+    uuid: str
+    category: int
+    name: str
+    description: str | None = None
+    is_public: bool
     create_time: datetime
-    update_time: datetime | None
+    update_time: datetime | None = None
+    config_json: str | None = None
+    creator: UserPublicInfo
+    engine_provided: EngineProvidedInfo
+
     @field_serializer("create_time")
-    def serializer_create_time(self, v: datetime):
+    def serializer_create_timezone(self, v: datetime):
         if v.tzinfo is None:
             return v.replace(tzinfo=timezone.utc)  # 默认转换为 UTC
         return v
     @field_serializer("update_time")
-    def serializer_update_time(self, v: datetime | None):
+    def serializer_update_timezone(self, v: datetime | None):
         if v is None:
             return None
         if v.tzinfo is None:
             return v.replace(tzinfo=timezone.utc)  # 默认转换为 UTC
         return v
+
+    class Config:
+        from_attributes = True
+        
+class EngineBaseInfo(BaseModel):
+    id: int
+    uuid: str
+    name: str
+    description: str | None = None
+    is_public: bool
+    create_time: datetime
+    update_time: datetime | None = None
+    is_forked: bool | None = None
+    creator: UserPublicInfo
+    engine_provided: EngineProvidedInfo
+    
+    @field_serializer("create_time")
+    def serializer_create_timezone(self, v: datetime):
+        if v.tzinfo is None:
+            return v.replace(tzinfo=timezone.utc)  # 默认转换为 UTC
+        return v
+    @field_serializer("update_time")
+    def serializer_update_timezone(self, v: datetime | None):
+        if v is None:
+            return None
+        if v.tzinfo is None:
+            return v.replace(tzinfo=timezone.utc)  # 默认转换为 UTC
+        return v
+
     class Config:
         from_attributes = True
 
-class EngineSearchRequest(BaseModel):
-    keyword: str
-    filter_category: int | None = None
+class EngineInfo(BaseModel):
+    id: int
+    uuid: str
+    category: int
+    name: str
+    description: str | None = None
+    is_public: bool
+    create_time: datetime
+    update_time: datetime | None = None
+    is_forked: bool | None = None
+    creator: UserPublicInfo
+    engine_provided: EngineProvidedInfo
+    
+    @field_serializer("create_time")
+    def serializer_create_timezone(self, v: datetime):
+        if v.tzinfo is None:
+            return v.replace(tzinfo=timezone.utc)  # 默认转换为 UTC
+        return v
+    @field_serializer("update_time")
+    def serializer_update_timezone(self, v: datetime | None):
+        if v is None:
+            return None
+        if v.tzinfo is None:
+            return v.replace(tzinfo=timezone.utc)  # 默认转换为 UTC
+        return v
 
-class MineEngineSearchResponse(BaseModel):
-    data: list[UserEngineInfo]
+    class Config:
+        from_attributes = True
 
-class ProvideEngineSearchResponse(BaseModel):
+class UsableEnginesResponse(BaseModel):
     data: list[EngineInfo]
     class Config:
         from_attributes = True
+
+class UsableEngineSearchRequest(BaseModel):
+    keyword: str | None = None
+    filter_category: int | None = None
+
+class CommunityEngineSearchRequest(BaseModel):
+    keyword: str | None = None
+    start: int | None = None
+    limit: int = 10
+    filter_category: int | None = None
+
+class EngineDeleteRequest(BaseModel):
+    engine_id: int
+
+class EngineForkRequest(BaseModel):
+    engine_id: int
+    status: bool
+
+class EngineUpdateRequest(BaseModel):
+    engine_id: int
+    config_json: str| None = None
+    name: str | None = None
+    description: str | None = None
+    is_public: bool | None = None
