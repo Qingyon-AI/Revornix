95dc691766d83df5d94d4497f70ea1ed649bcd46
Kinda Hall <1142704468@qq.com>
2026-01-31 17:17:55 +0800

    fix: 🐛 Fix the data/sql/create

--- api/data/sql/clear.py
+++ api/data/sql/clear.py
@@ -1,6 +1,6 @@
 from common.logger import info_logger
 from sqlalchemy import text
-from data.sql.base import sqlalchemy_engine
+from data.sql.base import engine
 
 # =========================================================
 # PostgreSQL：安全清库
@@ -8,7 +8,7 @@ from data.sql.base import sqlalchemy_engine
 
 def drop_schema_postgres():
     info_logger.warning("⚠️ Dropping ALL tables via DROP SCHEMA public CASCADE...")
-    with sqlalchemy_engine.connect() as conn:
+    with engine.connect() as conn:
         conn.execute(text("DROP SCHEMA IF EXISTS public CASCADE;"))
         conn.execute(text("CREATE SCHEMA public;"))
         conn.execute(text("GRANT ALL ON SCHEMA public TO public;"))
