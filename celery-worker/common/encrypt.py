49df951ec4b856d351af45f2725bd3f2e6b2f45e
Kinda Hall <1142704468@qq.com>
2026-02-04 01:20:59 +0800

    feat: 🎸 Add some notification-related envs

--- celery-worker/common/encrypt.py
+++ celery-worker/common/encrypt.py
@@ -6,6 +6,8 @@ from cryptography.hazmat.primitives.ciphers.aead import AESGCM
 APIKEY_ENCRYPT_KEY = os.environ.get("APIKEY_ENCRYPT_KEY")
 ENGINE_CONFIG_ENCRYPT_KEY = os.environ.get("ENGINE_CONFIG_ENCRYPT_KEY")
 FILE_SYSTEM_CONFIG_ENCRYPT_KEY = os.environ.get("FILE_SYSTEM_CONFIG_ENCRYPT_KEY")
+NOTIFICATION_SOURCE_CONFIG_ENCRYPT_KEY = os.environ.get("NOTIFICATION_SOURCE_CONFIG_ENCRYPT_KEY")
+NOTIFICATION_TARGET_CONFIG_ENCRYPT_KEY = os.environ.get("NOTIFICATION_TARGET_CONFIG_ENCRYPT_KEY")
 
 if not APIKEY_ENCRYPT_KEY:
     raise Exception("APIKEY_ENCRYPT_KEY not set")
@@ -13,11 +15,17 @@ if not ENGINE_CONFIG_ENCRYPT_KEY:
     raise Exception("ENGINE_CONFIG_ENCRYPT_KEY not set")
 if not FILE_SYSTEM_CONFIG_ENCRYPT_KEY:
     raise Exception("FILE_SYSTEM_CONFIG_ENCRYPT_KEY not set")
+if not NOTIFICATION_SOURCE_CONFIG_ENCRYPT_KEY:
+    raise Exception("NOTIFICATION_SOURCE_CONFIG_ENCRYPT_KEY not set")
+if not NOTIFICATION_TARGET_CONFIG_ENCRYPT_KEY:
+    raise Exception("NOTIFICATION_TARGET_CONFIG_ENCRYPT_KEY not set")
 
 # 系统环境变量里的主密钥
 APIKEY_MASTER_KEY = base64.b64decode(APIKEY_ENCRYPT_KEY)
 ENGINE_CONFIG_MASTER_KEY = base64.b64decode(ENGINE_CONFIG_ENCRYPT_KEY)
 FILE_SYSTEM_CONFIG_MASTER_KEY = base64.b64decode(FILE_SYSTEM_CONFIG_ENCRYPT_KEY)
+NOTIFICATION_SOURCE_CONFIG_MASTER_KEY = base64.b64decode(NOTIFICATION_SOURCE_CONFIG_ENCRYPT_KEY)
+NOTIFICATION_TARGET_CONFIG_MASTER_KEY = base64.b64decode(NOTIFICATION_TARGET_CONFIG_ENCRYPT_KEY)
 
 def encrypt_file_system_config(
     file_system_config_json_str: str
@@ -65,6 +73,38 @@ def decrypt_api_key(
     raw = base64.b64decode(encoded)
     nonce, ciphertext = raw[:12], raw[12:]
     aesgcm = AESGCM(APIKEY_MASTER_KEY)
+    return aesgcm.decrypt(nonce, ciphertext, None).decode()
+
+def encrypt_notification_source_config(
+    config_json_str: str
+):
+    aesgcm = AESGCM(NOTIFICATION_SOURCE_CONFIG_MASTER_KEY)
+    nonce = os.urandom(12)
+    ciphertext = aesgcm.encrypt(nonce, config_json_str.encode(), None)
+    return base64.b64encode(nonce + ciphertext).decode()
+
+def decrypt_notification_source_config(
+    encoded: str
+):
+    raw = base64.b64decode(encoded)
+    nonce, ciphertext = raw[:12], raw[12:]
+    aesgcm = AESGCM(NOTIFICATION_SOURCE_CONFIG_MASTER_KEY)
+    return aesgcm.decrypt(nonce, ciphertext, None).decode()
+
+def encrypt_notification_target_config(
+    config_json_str: str
+):
+    aesgcm = AESGCM(NOTIFICATION_TARGET_CONFIG_MASTER_KEY)
+    nonce = os.urandom(12)
+    ciphertext = aesgcm.encrypt(nonce, config_json_str.encode(), None)
+    return base64.b64encode(nonce + ciphertext).decode()
+
+def decrypt_notification_target_config(
+    encoded: str
+):
+    raw = base64.b64decode(encoded)
+    nonce, ciphertext = raw[:12], raw[12:]
+    aesgcm = AESGCM(NOTIFICATION_TARGET_CONFIG_MASTER_KEY)
     return aesgcm.decrypt(nonce, ciphertext, None).decode()
 
 if __name__ == "__main__":
